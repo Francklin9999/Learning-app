362686f52b3038f4dcfd6e511cc45384db9b7e21
Francklin9999 <162392919+Francklin9999@users.noreply.github.com>
2024-06-28 20:10:53 -0400

    Home page

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css'
+import './App.css';
+import Home from './component/Home';
+import Login from './component/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
 
   return (
     <>
-      
+      <Router>
+        
+        <Routes>
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+
+      </Router>
     </>
-  )
+  );
 }
 
