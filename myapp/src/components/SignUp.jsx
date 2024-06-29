fe72f95eebb91cf7845a9975633c56022c042afc
Francklin9999 <162392919+Francklin9999@users.noreply.github.com>
2024-06-29 02:05:34 -0400

    SignUp Start

---
+++ myapp/src/components/SignUp.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import '../styles/SignUp';
+
+export default function SignUp() {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const SignUpData = {
+            email,
+            username,
+        }
+    }
+
+    return (
+        <div className="SignUp">
+            <form onSumbit={handleSubmit}>
+                <div className="email">
+                    <label htmlFor="email">Email</label>
+                    <input
+                    type="email"
+                    name="email" 
+                    id ="email"
+                    value={email} onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="username">
+                    <label htmlFor="username">Username</label>
+                    <input
+                    type="text"
+                    name="username" 
+                    id ="username"
+                    value={username} onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="password">
+                    <label htmlFor="password">Password</label>
+                    <input
+                    type="password"
+                    name="password" 
+                    id ="password"
+                    value={password} onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+            </form>
+        </div>
+    )
+}
