a2d9259aaf860817e98947f97cec316466ba5511
Francklin9999 <162392919+Francklin9999@users.noreply.github.com>
2024-07-12 03:25:08 -0400

    Learn responsive fix

--- myapp/src/components/Learn.jsx
+++ myapp/src/components/Learn.jsx
@@ -1,11 +1,14 @@
 import {title, language} from '../const';
 import { useState } from 'react';
 import Sidebar from './Sidebar';
-import Footer from './Footer';
 import '../styles/Learn.css';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Learn() {
+
+    const navigate = useNavigate();
+
     const [isOpen, setIsOpen] = useState(false);
     const [selectedLevel, setSelectedLevel] = useState(null);
     const [selectedLanguage, setSelectedLanguage] = useState(null);
@@ -36,19 +39,26 @@ export default function Learn() {
         if(selectedLevel == null || selectedLanguage == null) {
             alert('Please select a level and language');
         } else {
-            window.location.href = '/lesson';
+            navigate('/lesson', { 
+                state: { 
+                    level : selectedLevel,
+                    language : selectedLanguage,
+                }
+            });
         }
     }
     
     return (
         <>
+
+            <Sidebar/>
             
             <header className="learn-header">
                     <h1>{title}</h1>
             </header>
 
-            <main className="container-fluid learn-main">
-                <div className="col learn-roadmap-container">
+            <main className="learn-main">
+                <div className="learn-roadmap-container">
                     {milestones.map((milestone, index) => (
                     <div key={index} className="learn-roadmap-item" id={(index % 2 === 0 || index === 0) ? 'learn-roadmap-items-even' : 'learn-roadmap-items-odd'}>
                     <div className="learn-roadmap-icon">
@@ -63,10 +73,10 @@ export default function Learn() {
                     </div>
                     ))}
                 </div>
-                <div className="col learn-button-container">
+                <div className="learn-button-container">
                     <div className="learn-dropdown">
                         <div className="learn-dropdown-options">
-                        Select a Language:
+                        <h3>Select a Language:</h3>
                         <button onClick={toggleDropdown} className="learn-dropdown-toggle">
                             {selectedLanguage || 'Select'}
                         </button>
@@ -81,13 +91,20 @@ export default function Learn() {
                         )}
                         </div>
                     </div>
-                <div>
-                    <button onClick={goToLesson}>Go to lesson</button>
-                </div>
+                    <div>
+                        <div className="learn-gotolesson">
+                            <button onClick={goToLesson}>
+                                <span>Go to lesson</span>
+                                <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="37" cy="37" r="35.5" stroke="black" stroke-width="3"></circle>
+                                    <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </main>
 
-            <Sidebar/>
         </>
     );
 }
