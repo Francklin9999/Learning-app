a8fba94a5b839e3e9183e61a8b4ab7ae9c314aea
Francklin9999 <162392919+Francklin9999@users.noreply.github.com>
2024-07-14 02:45:44 -0400

    Password reset fix

--- myapp/src/components/PasswordReset.jsx
+++ myapp/src/components/PasswordReset.jsx
@@ -1,20 +1,44 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/PasswordReset.css';
 
 export default function PasswordReset() {
+    const navigate = useNavigate();
+
+    const [showMessage, setShowMessage] = useState(false);
+
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        setShowMessage(true);
+        setTimeout (() => {
+            navigate('/');
+        }, 10000);
+    };
+
     return (
-        <div className="passwordreset-form-container">
-            <div className="passwordreset-logo-container">Forgot Password</div>
+            <>
+            {showMessage ? (
+            <div className="passwordreset-reset">
+                <h3>An email has been sent!</h3>
+                <p>Check your email for a link to reset your password.</p>
+                <p>You will be redirected to the home page shortly.</p>
+            </div>
+            ) : (
+                <div className="passwordreset-form-container">
+                <div className="passwordreset-logo-container">Forgot Password</div>
 
-            <form className="passwordreset-form">
-                <div className="passwordreset-form-group">
-                    <label for="email">Email</label>
-                <input type="text" id="passwordreset-email" name="email" placeholder="Enter your email" required/>
+                <form className="passwordreset-form" onSubmit={handleSumbit}>
+                    <div className="passwordreset-form-group">
+                        <label for="email">Email</label>
+                    <input type="text" id="passwordreset-email" name="email" placeholder="Enter your email" required autocomplete="email"/>
+                    </div>
+
+                    <button className="passwordreset-form-submit-btn" type="submit">Send Email</button>
+                </form>
+
+                <p className="passwordreset-signup-link">Don't have an account?<a href="/SignUp" className="passwordreset-signup-link link"> Sign up now</a></p>
                 </div>
-
-                <button className="passwordreset-form-submit-btn" type="submit">Send Email</button>
-            </form>
-
-            <p className="passwordreset-signup-link">Don't have an account?<a href="/SignUp" className="passwordreset-signup-link link"> Sign up now</a></p>
-        </div>
-    )
-}
+        )}
+        </>
+    );
+};
